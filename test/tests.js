47628d75b48e2e1ca56b7f3309decd0fa36e8832
alu0100697414 <alu0100697414@ull.edu.es>
2015-10-13 23:12:07 +0100

    Actualizados los test

--- test/tests.js
+++ test/tests.js
@@ -5,47 +5,28 @@ describe("Test para ConverTemp", function() {
   var fin = document.getElementById("resultado");
 
     it("Debería ser: 32e4F", function() {
-      var temp = new Temperatura(32,4,"F");
-      expect(temp.get_valor()).to.equal(32);
-      expect(temp.get_exp()).to.equal(4);
+      var temp = new Temperatura(32e4,"F");
+      expect(temp.get_valor()).to.equal(32e4);
       expect(temp.get_tipo()).to.equal("F");
     });
 
     it("Debería ser: -4.3e-2C", function() {
-      var temp = new Temperatura(-4.3,-2,"C");
-      expect(temp.get_valor()).to.equal(-4.3);
-      expect(temp.get_exp()).to.equal(-2);
+      var temp = new Temperatura(-4.3e-2,"C");
+      expect(temp.get_valor()).to.equal(-4.3e-2);
       expect(temp.get_tipo()).to.equal("C");
     });
 
     it("Debería ser: 3.2e4F", function() {
-      var temp = new Temperatura(0,0,"C");
-      temp.set_valor(3.2);
-      temp.set_exp(4);
+      var temp = new Temperatura(0,"C");
+      temp.set_valor(3.2e4);
       temp.set_tipo("F");
-      expect(temp.get_valor()).to.equal(3.2);
-      expect(temp.get_exp()).to.equal(4);
+      expect(temp.get_valor()).to.equal(3.2e4);
       expect(temp.get_tipo()).to.equal("F");
-    });
-
-    it("3.2e-2 === 0.032", function() {
-      var temp = new Temperatura(3.2,-2,"C");
-      temp.set_valor(parseFloat(temp.get_valor()));
-      temp.calculo_numero();
-      expect(temp.get_valor()).to.equal(0.032);
-    });
-
-    it("3.2e2 === 320", function() {
-      var temp = new Temperatura(3.2,2,"C");
-      temp.set_valor(parseFloat(temp.get_valor()));
-      temp.calculo_numero();
-      expect(temp.get_valor()).to.equal(320);
     });
 
     it("0.032C === 32.0576F", function() {
       var temp = new Temperatura();
       temp.set_valor(0.032);
-      temp.set_exp(0);
       temp.set_tipo("C");
       var res = temp.to_f();
       expect(res).to.equal(32.0576);
@@ -54,7 +35,6 @@ describe("Test para ConverTemp", function() {
     it("32,0576F === 0.032C", function() {
       var temp = new Temperatura();
       temp.set_valor(32.0576);
-      temp.set_exp(0);
       temp.set_tipo("F");
       var res = temp.to_c();
       expect(res).to.equal(0.032000000000000424);
