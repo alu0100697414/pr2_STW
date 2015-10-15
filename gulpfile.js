2ffe8b319ae7ce566677766864ef446943f6c940
alu0100697414 <alu0100697414@ull.edu.es>
2015-10-15 15:55:24 +0100

    Alguna modificacion

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp    = require('gulp'),
     uglify  = require('gulp-uglify'),
     concat  = require('gulp-concat');
 var del     = require('del');
+var open = require('gulp-open');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
 
@@ -22,4 +23,9 @@ gulp.task('minify', function () {
 
 gulp.task('clean', function(cb) {
   del(['minified/*'], cb);
+});
+
+gulp.task("test", function(){
+  gulp.src('')
+    .pipe(open({ uri: 'https://alu0100697414.github.io/pr2_STW/test/'}));
 });
